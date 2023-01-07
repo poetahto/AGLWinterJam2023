dcbd21becbc56074645e53e611af7bf64cced1e0
Daniel Walls <walls.dk@gmail.com>
2023-01-07 14:27:22 -0800

    appear strategies

---
+++ Assets/Scripts/EnableAppear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu]
+    public class EnableAppear : AppearStrategy
+    {
+        [SerializeField] private float appearDelay;
+
+        public override IEnumerator Apply(List<GameObject> platforms)
+        {
+            foreach (var gameObject in platforms)
+                gameObject.SetActive(true);
+
+            yield break;
+        }
+
+        public override void Initialize(List<GameObject> platforms)
+        {
+            foreach (var gameObject in platforms)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlatformGroup.cs
+++ Assets/Scripts/PlatformGroup.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class PlatformGroup : MonoBehaviour
     {
+        [RuntimeInitializeOnLoadMethod]
+        private static void Init()
+        {
+            LeanTween.reset();
+        }
         
+        [SerializeField] private AppearStrategy strategy;
+
+        private List<GameObject> _children = new List<GameObject>();
+        private bool _isVisible;
+
+        private void Awake()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+                _children.Add(transform.GetChild(i).gameObject);
+            
+            strategy.Initialize(_children);
+        }
+
+        public void Show()
+        {
+            if (_isVisible)
+                return;
+            
+            StartCoroutine(strategy.Apply(_children));
+            _isVisible = true;
+        }
     }
 }
